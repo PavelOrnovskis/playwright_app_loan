b2e640e591a01809bcd3e008ca013100b867ada0
pavel <pavel.ornovskis@gmail.com>
2025-03-11 19:14:48 +0200

    new

---
+++ page-objects/atoms/Button.ts
@@ -0,0 +1,27 @@
+import {Locator, Page} from "@playwright/test";
+import {SmallLoanPage} from "../pages/SmallLoanPage";
+
+export class Button {
+    readonly page: Page
+    readonly dataTestId: string
+
+    constructor(page: Page, dataTestId: string) {
+        this.page = page
+        this.dataTestId = dataTestId
+
+    }
+
+    get button(): Locator {
+        return this.page.getByTestId(this.dataTestId)
+
+    }
+
+    async click(): Promise<void> {
+        await this.button.click();
+
+    }
+
+
+
+
+}
